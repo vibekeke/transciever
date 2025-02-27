e003ba02c67cf7274bf367020bcfd13e9e3ded05
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-02-27 20:03:37 +0100

    arrow implementation started
    woho

--- interaction/interaction area/interaction_area.gd
+++ interaction/interaction area/interaction_area.gd
@@ -1,7 +1,11 @@
 extends Area2D
 class_name InteractionArea
 
+
 @export var action_name: String = "interact"
+@export var arrow_location: int = 20
+
+@onready var arrow = $Arrow
 
 var interact: Callable = func():
 	pass
@@ -10,9 +14,23 @@ var interact: Callable = func():
 	#men override den med en helt annen funksjon. Elns, skjønte ikke helt.
 
 
+func _ready() -> void:
+	arrow.position.y -= arrow_location
+	arrow.hide()
+
+func _process(delta):
+	if InteractionManager.active_areas.size() > 0:
+		if InteractionManager.active_areas[0] == self:
+			arrow.show()
+		else:
+			arrow.hide()
+
 func _on_body_entered(body: Node2D) -> void:
 	InteractionManager.register_area(self)
 
 
 func _on_body_exited(body: Node2D) -> void:
 	InteractionManager.unregister_area(self)
+
+
+	
--- interaction/interaction area/interaction_manager.gd
+++ interaction/interaction area/interaction_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var label = $Label
+@onready var arrow = $Arrow
 
 const base_text = "[Space] to "
 
@@ -20,13 +21,17 @@ func unregister_area(area: InteractionArea):
 func _process(delta):
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
+		
+		'''
 		label.text = base_text + active_areas[0].action_name
 		label.global_position = active_areas[0].global_position
 		label.global_position.y -= 100
 		label.global_position.x -= label.size.x / 2
 		label.show()
+		
 	else:
 		label.hide()
+	'''
 
 func _sort_by_distance_to_player(area1, area2):
 	var area1_to_player = player.global_position.distance_squared_to(area1.global_position)
@@ -37,7 +42,7 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_accept") && can_interact:
 		if active_areas.size() > 0:
 			can_interact = false
-			label.hide()
+			#label.hide()
 			
 			await active_areas[0].interact.call() #Kul greie.
 			
